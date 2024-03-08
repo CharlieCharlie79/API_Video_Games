536ae1a40afbcc88da7a1b28693cf63599120eb3
CharlieCharlie79 <carloswork067@hotmail.com>
2024-03-08 06:24:32 -0500

    Get the names from the API

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,6 +1,38 @@
-const API_KEY = 'd1d436f671144fe58cd8bad3d2d6e457';
+const API_Key = 'd1d436f671144fe58cd8bad3d2d6e457';
 
-fetch(`https://api.rawg.io/api/games?key=${API_KEY}`)
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
+/*
+fetch(`https://api.rawg.io/api/platforms?key=${API_Key}`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+
+        return response.json();
+    })
+    .then(data => {
+        console.log("Video Games Data: ", data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+*/
+
+getListOfGames();
+
+async function getListOfGames() {
+    const response = await fetch(`https://api.rawg.io/api/platforms?key=${API_Key}`)
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    length = data.results.length;
+
+    // gamesList = data.results.games.name;
+
+    console.log(data);
+    var temp = '';
+    for (i = 0; i < length; i++) {
+        temp += "<tr>";
+        temp += "<td>" + data.results[i].games[0].name + "</td>";
+    }
+    document.getElementById("container").innerHTML = temp;
+}
